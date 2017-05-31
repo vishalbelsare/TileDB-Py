998a65d676f6703e95b96b27ba430e5b7a8535ea
Jake Bolewski <jakebolewski@gmail.com>
2017-05-30 21:14:49 -0400

    WIP

--- tiledb/__init__.py
+++ tiledb/__init__.py
@@ -1,7 +1,10 @@
+import sys
 import ctypes
+from os import path
+
+libtiledb = ctypes.cdll.LoadLibrary(path.join(sys.prefix, 'lib', 'libtiledb.dylib'))
 
 def _version():
-    libtiledb = ctypes.cdll.LoadLibrary("libtiledb.dylib")
     major, minor, rev = ctypes.c_int(0), ctypes.c_int(0), ctypes.c_int(0)
     libtiledb.tiledb_version(ctypes.byref(major), ctypes.byref(minor), ctypes.byref(rev))
     return (major.value, minor.value, rev.value)
